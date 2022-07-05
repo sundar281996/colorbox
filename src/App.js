143834fc33d322e813b4ba81e1834d3bc0e23b67
sundar281996 <rsundar1996@gmail.com>
2022-07-05 13:05:31 +0530

    colorbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+  <Colorbox/>
     </div>
   );
+}
+function Colorbox(){
+  const [color, setColor]=useState("skyblue")
+  const [colorlist,setColorlist] =useState(["red","blue","orange"])
+  const styles={
+    fontSize:"24px",
+    backgroundColor:color
+  }
+  return(
+   <div>
+   <div className='sun'>
+    <input onChange={(event)=>setColor(event.target.value)} style={styles} value={color} type="text"></input>
+    <button onClick={()=>setColorlist([...colorlist,color])}>add color</button>
+
+  
+    </div>
+    {colorlist.map((clr)=> <Addcolor color={clr} />
+    )}
+    </div>
+  )
+}
+function Addcolor({color}){
+  const styles={
+    backgroundColor:color,
+    height:"35px",
+    width:"300px",
+    marginTop:"30px"
+  }
+  return(
+    <div style={styles}>
+
+    </div>
+  )
 }
 
 export default App;
